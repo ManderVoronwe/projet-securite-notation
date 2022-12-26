47e53bd448479caeaff3a64c9fe762e40ba9d16a
ManderVoronwe <95370877+ManderVoronwe@users.noreply.github.com>
2022-12-26 12:59:39 +0100

    il y a un graphique
    changement de du format de sauvegarde
    changement de l'architecture pour une accessibilité extérieur

---
+++ webInterface/src/data/data.js
@@ -0,0 +1,32 @@
+const data ={releve :[
+    {
+        "rnumber": 1 ,
+        "processus": 4 ,
+        "advertising": 10,
+        "point": 14
+    },
+    {
+        "rnumber": 2 ,
+        "processus": 4 ,
+        "advertising": 10,
+        "point": 14
+    },
+    {
+        "rnumber": 3 ,
+        "processus": 4 ,
+        "advertising": 10,
+        "point": 14
+    },
+    {
+        "rnumber": 3 ,
+        "processus": 4 ,
+        "advertising": 10,
+        "point": 13
+    },
+    {
+        "rnumber": 3 ,
+        "processus": 4 ,
+        "advertising": 10,
+        "point": -3
+    }
+]};

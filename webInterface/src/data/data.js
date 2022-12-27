fb2ced5f3b9a278c5b6055a99de2c630c3cb9a6a
ManderVoronwe <95370877+ManderVoronwe@users.noreply.github.com>
2022-12-27 12:53:38 +0100

    bit of styling

--- webInterface/src/data/data.js
+++ webInterface/src/data/data.js
@@ -18,13 +18,19 @@ const data ={releve :[
         "point": 14
     },
     {
-        "rnumber": 3 ,
+        "rnumber": 4 ,
         "processus": 4 ,
         "advertising": 10,
         "point": 13
     },
     {
-        "rnumber": 3 ,
+        "rnumber": 5 ,
+        "processus": 4 ,
+        "advertising": 10,
+        "point": -3
+    },
+    {
+        "rnumber": 6 ,
         "processus": 4 ,
         "advertising": 10,
         "point": -3

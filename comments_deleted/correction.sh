f23b077e61fd79c0e18207e21bb91988454a9d55
mobaka <mohammedbakati@live.fr>
2022-12-31 14:32:19 +0100

    maj parametres

--- comments_deleted/correction.sh
+++ comments_deleted/correction.sh
@@ -2,13 +2,16 @@
 i=0
 
 cat /dev/null > tmp_requete.txt
+USER=$1
+PASSWORD=$2
+SERVER=$3
+DB_name=$4
 
 #ma liste de commentaires temporaires reviews.txt
 while read line  
 do   
-	echo $line
-	#$line="$(tr -d "\n" <<< "$line")" 
-	mysql -h 127.0.0.1 -u root -B restau-from-git -e "select review from review where review='$line'" > tmp_requete.txt
+
+	mysql -h $SERVER -u $USER -p$PASSWORD -B $DB_name -e "select review from review where review='$line'" > tmp_requete.txt
 	
 	test -s tmp_requete.txt
 	returnval="$?"

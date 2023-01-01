1451838d4e56ce1530caced90ce3e2b3bbf73a56
AlexTheGeek <albrunet2000@lapinfo.fr>
2023-01-01 14:09:32 +0100

    check

--- checkProcessus/ProcessusGeter.sh
+++ checkProcessus/ProcessusGeter.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 SERVER=$1
-CERTIFICATE=/app/cert/sshdocker
-PROCESSUS=(lighttpd bck php8)
+CERTIFICATE="/app/cert/sshdocker"
+PROCESSUS=(lighttpd bck php8 dock crypt)
 number=0
 for i in $processus
 do
     number=$(($number+$(ssh -i $CERTIFICATE root@$SERVER "ps -aux" | grep $i | wc -l) ))
 done
 
-echo $number
+echo $((-1 * $number))
 

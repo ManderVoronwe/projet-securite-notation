26456b523aad6c823a7de74c6a9772eec4d76ace
ManderVoronwe <95370877+ManderVoronwe@users.noreply.github.com>
2023-01-04 08:56:27 +0100

    ...

--- webInterface/src/data/data.js
+++ webInterface/src/data/data.js
@@ -3,36 +3,20 @@ const  DataJson ={"releve" :[
         "rnumber": 1 ,
         "processus": 4 ,
         "advertising": 10,
-        "point": 14
+        "point": 14,
+        "DBonline": -200,
+        "DBaccess": 0,
+        "TauxDeFauxComm" : 0.5,
+        "NombreDeVraisCommDel" : 0,
     },
     {
         "rnumber": 2 ,
         "processus": 4 ,
         "advertising": 10,
-        "point": 14
+        "point": 14,
+        "DBonline": -200,
+        "DBaccess": 0,
+        "TauxDeFauxComm" : 0.3,
+        "NombreDeVraisCommDel" : 5,
     },
-    {
-        "rnumber": 3 ,
-        "processus": 4 ,
-        "advertising": 10,
-        "point": 14
-    },
-    {
-        "rnumber": 4 ,
-        "processus": 4 ,
-        "advertising": 10,
-        "point": 13
-    },
-    {
-        "rnumber": 5 ,
-        "processus": 4 ,
-        "advertising": 10,
-        "point": -3
-    },
-    {
-        "rnumber": 6 ,
-        "processus": 4 ,
-        "advertising": 10,
-        "point": -3
-    }
 ]};
